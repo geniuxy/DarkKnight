ce1be5ad9317ae3db6c651623f5faf558a3c9d32
geniuxy <2238384910@qq.com>
2025-12-05 20:29:57 +0800

    Frontend Player Controller

---
+++ Source/DarkKnight/Public/Controllers/DkFrontendPlayerController.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "DkFrontendPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DARKKNIGHT_API ADkFrontendPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};

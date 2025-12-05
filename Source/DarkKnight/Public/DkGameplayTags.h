7dd92566fe49888c9f04e6f34e79d8ee2c091992
geniuxy <2238384910@qq.com>
2025-12-05 20:48:31 +0800

    Widget Stack Gameplay Tags

---
+++ Source/DarkKnight/Public/DkGameplayTags.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace DkGameplayTags
+{
+	DARKKNIGHT_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Dk_WidgetStack_Modal);
+	DARKKNIGHT_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Dk_WidgetStack_GameMenu);
+	DARKKNIGHT_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Dk_WidgetStack_GameHud);
+	DARKKNIGHT_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Dk_WidgetStack_Frontend);
+}

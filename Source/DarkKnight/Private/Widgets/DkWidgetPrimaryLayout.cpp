826736c73d3651e425d339ad564d1a898ad5b749
geniuxy <2238384910@qq.com>
2025-12-06 15:46:07 +0800

    Debug Helper

--- Source/DarkKnight/Private/Widgets/DkWidgetPrimaryLayout.cpp
+++ Source/DarkKnight/Private/Widgets/DkWidgetPrimaryLayout.cpp
@@ -3,11 +3,13 @@
 
 #include "Widgets/DkWidgetPrimaryLayout.h"
 
+#include "DarkKnightDebugHelper.h"
+
 UCommonActivatableWidgetContainerBase* UDkWidgetPrimaryLayout::FindWidgetStackByTag(const FGameplayTag& InTag) const
 {
 	checkf(RegisteredWidgetStackMap.Contains(InTag), TEXT("无法通过Tag(%s)找到Widget Stack"), *InTag.ToString());
 
-	return RegisteredWidgetStackMap.FindRef(InTag); //FindRef只对外提供副本(“只读且安全”的查询)
+	return RegisteredWidgetStackMap.FindRef(InTag); // FindRef只对外提供副本(“只读且安全”的查询)
 }
 
 void UDkWidgetPrimaryLayout::RegisterWidgetStack(FGameplayTag InStackTag,
@@ -18,6 +20,8 @@ void UDkWidgetPrimaryLayout::RegisterWidgetStack(FGameplayTag InStackTag,
 		if (!RegisteredWidgetStackMap.Contains(InStackTag))
 		{
 			RegisteredWidgetStackMap.Add(InStackTag, InStack);
+
+			Debug::Print(FString::Printf(TEXT("Widget Stack 已通过 Tag(%s) 注册！"), *InStackTag.ToString()));
 		}
 	}
 }

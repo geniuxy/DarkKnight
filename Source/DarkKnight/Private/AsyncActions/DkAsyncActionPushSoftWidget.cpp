1f2e22f9ef0ba7591627e6334ceaadc4de12b168
geniuxy <2238384910@qq.com>
2025-12-07 16:33:55 +0800

    BP Aysnc Action

---
+++ Source/DarkKnight/Private/AsyncActions/DkAsyncActionPushSoftWidget.cpp
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AsyncActions/DkAsyncActionPushSoftWidget.h"
+
+#include "GameplayTagContainer.h"
+
+UDkAsyncActionPushSoftWidget* UDkAsyncActionPushSoftWidget::PushSoftWidget(
+	const UObject* WorldContextObject,
+	APlayerController* OwningPlayerController,
+	TSoftClassPtr<UDkWidgetActivatableBase> InSoftWidgetClass,
+	FGameplayTag InWidgetStackTag,
+	bool bFocusOnNewlyPushedWidget)
+{
+	checkf(!InSoftWidgetClass.IsNull(), TEXT("PushSoftWidgetToStack需要一个非空的入参InSoftWidgetClass"));
+
+	if (GEngine)
+	{
+		if (UWorld* World =
+			GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
+		{
+			UDkAsyncActionPushSoftWidget* Node = NewObject<UDkAsyncActionPushSoftWidget>();
+
+			Node->RegisterWithGameInstance(World);
+
+			return Node;
+		}
+	}
+	return nullptr;
+}
---
+++ Source/DarkKnight/Public/AsyncActions/DkAsyncActionPushSoftWidget.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "DkAsyncActionPushSoftWidget.generated.h"
+
+struct FGameplayTag;
+class UDkWidgetActivatableBase;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPushSoftWidgetDelegate, UDkWidgetActivatableBase*, PushedWidget);
+
+/**
+ * 
+ */
+UCLASS()
+class DARKKNIGHT_API UDkAsyncActionPushSoftWidget : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, meta = ( WorldContext = "WorldContextObject", HidePin = "WorldContextObject",
+		BlueprintInternalUseOnly = "true", DisPlayName = "Push Soft Widget To Widget Stack"))
+	static UDkAsyncActionPushSoftWidget* PushSoftWidget(
+		const UObject* WorldContextObject,
+		APlayerController* OwningPlayerController,
+		TSoftClassPtr<UDkWidgetActivatableBase> InSoftWidgetClass,
+		UPARAM(meta = (Categories = "Dk.WidgetStack")) FGameplayTag InWidgetStackTag,
+		bool bFocusOnNewlyPushedWidget = true
+	);
+
+	UPROPERTY(BlueprintAssignable)
+	FOnPushSoftWidgetDelegate OnWidgetCreatedBeforePush;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnPushSoftWidgetDelegate AfterPush;
+};

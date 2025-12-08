02cda1b64bacaf502edb55f11e3435c29db0b1d8
geniuxy <2238384910@qq.com>
2025-12-08 19:05:29 +0800

    Developer Settings

---
+++ Source/DarkKnight/Public/DeveloperSettings/DkUIDeveloperSettings.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DeveloperSettings.h"
+#include "DkUIDeveloperSettings.generated.h"
+
+class UDkWidgetActivatableBase;
+/**
+ * 
+ */
+UCLASS(Config = Game, DefaultConfig, meta = (DisplayName = "DarkKnight UI Settings"))
+class DARKKNIGHT_API UDkUIDeveloperSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(Config, EditAnywhere, Category = "Widget Reference", meta = (ForceInlineRow, Categories = "Dk.Widget"))
+	TMap<FGameplayTag, TSoftClassPtr<UDkWidgetActivatableBase>> FrontendWidgetMap;
+};

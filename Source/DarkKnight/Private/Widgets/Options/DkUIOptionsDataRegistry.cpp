d65a6cb96ae80e2ff140b00e944c988dde60fbe4
geniuxy <2238384910@qq.com>
2025-12-21 16:34:59 +0800

    Options Data Registry

---
+++ Source/DarkKnight/Private/Widgets/Options/DkUIOptionsDataRegistry.cpp
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/Options/DkUIOptionsDataRegistry.h"
+
+#include "Widgets/Options/DataObjects/DkUIListDataObjectCollection.h"
+
+void UDkUIOptionsDataRegistry::InitOptionsDataRegister(ULocalPlayer* InOwningLocalPlayer)
+{
+	InitGameplayCollectionTab();
+	InitAudioCollectionTab();
+	InitVideoCollectionTab();
+	InitControlCollectionTab();
+}
+
+void UDkUIOptionsDataRegistry::InitGameplayCollectionTab()
+{
+	UDkUIListDataObjectCollection* GameplayTabCollection = NewObject<UDkUIListDataObjectCollection>();
+	GameplayTabCollection->SetDataID(FName("GameplayTabCollection"));
+	GameplayTabCollection->SetDataDisplayName(FText::FromString(TEXT("游戏性")));
+
+	RegisteredOptionsTabCollections.Add(GameplayTabCollection);
+}
+
+void UDkUIOptionsDataRegistry::InitAudioCollectionTab()
+{
+	UDkUIListDataObjectCollection* AudioTabCollection = NewObject<UDkUIListDataObjectCollection>();
+	AudioTabCollection->SetDataID(FName("AudioTabCollection"));
+	AudioTabCollection->SetDataDisplayName(FText::FromString(TEXT("音频")));
+
+	RegisteredOptionsTabCollections.Add(AudioTabCollection);
+}
+
+void UDkUIOptionsDataRegistry::InitVideoCollectionTab()
+{
+	UDkUIListDataObjectCollection* VideoTabCollection = NewObject<UDkUIListDataObjectCollection>();
+	VideoTabCollection->SetDataID(FName("VideoTabCollection"));
+	VideoTabCollection->SetDataDisplayName(FText::FromString(TEXT("图像")));
+
+	RegisteredOptionsTabCollections.Add(VideoTabCollection);
+}
+
+void UDkUIOptionsDataRegistry::InitControlCollectionTab()
+{
+	UDkUIListDataObjectCollection* ControlTabCollection = NewObject<UDkUIListDataObjectCollection>();
+	ControlTabCollection->SetDataID(FName("ControlTabCollection"));
+	ControlTabCollection->SetDataDisplayName(FText::FromString(TEXT("控制")));
+
+	RegisteredOptionsTabCollections.Add(ControlTabCollection);
+}
---
+++ Source/DarkKnight/Public/Widgets/Options/DkUIOptionsDataRegistry.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "DkUIOptionsDataRegistry.generated.h"
+
+class UDkUIListDataObjectCollection;
+/**
+ * 
+ */
+UCLASS()
+class DARKKNIGHT_API UDkUIOptionsDataRegistry : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	// 在创建 UOptionsDataRegistry 类型的对象之后，OptionScreen会立即调用此方法。
+	void InitOptionsDataRegister(ULocalPlayer* InOwningLocalPlayer);
+
+private:
+	void InitGameplayCollectionTab();
+	void InitAudioCollectionTab();
+	void InitVideoCollectionTab();
+	void InitControlCollectionTab();
+
+	UPROPERTY(Transient)
+	TArray<UDkUIListDataObjectCollection*> RegisteredOptionsTabCollections;
+
+public:
+	FORCEINLINE const TArray<UDkUIListDataObjectCollection*>& GetRegisteredOptionsTabCollections() const
+	{
+		return RegisteredOptionsTabCollections;
+	}
+};

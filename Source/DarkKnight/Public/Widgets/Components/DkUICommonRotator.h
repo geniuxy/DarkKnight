6228f4e5aa1c9c69ee4ebb35496c7b755868be8b
geniuxy <2238384910@qq.com>
2025-12-22 21:06:20 +0800

    String Entry Widget

---
+++ Source/DarkKnight/Public/Widgets/Components/DkUICommonRotator.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonRotator.h"
+#include "DkUICommonRotator.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, BlueprintType, meta = (DisableNativeTick))
+class DARKKNIGHT_API UDkUICommonRotator : public UCommonRotator
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/DarkKnight/Public/Widgets/Options/ListEntries/DkUIWidgetListEntryString.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/Options/DkUIWidgetListEntryBase.h"
+#include "DkUIWidgetListEntryString.generated.h"
+
+class UDkUICommonRotator;
+class UDkUICommonButtonBase;
+/**
+ * 
+ */
+UCLASS()
+class DARKKNIGHT_API UDkUIWidgetListEntryString : public UDkUIWidgetListEntryBase
+{
+	GENERATED_BODY()
+
+private:
+	//***** Bound Widgets *****//
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget, AllowPrivateAccess = "true"))
+	UDkUICommonButtonBase* CommonButton_Previous;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget, AllowPrivateAccess = "true"))
+	UDkUICommonRotator* CommonRotator_AvailableOptions;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget, AllowPrivateAccess = "true"))
+	UDkUICommonButtonBase* CommonButton_Next;
+	//***** Bound Widgets *****//
+};

1c4cf6e2f880b995b0eb4abf6ecb641e2b0b03ed
geniuxy <2238384910@qq.com>
2025-12-30 20:15:52 +0800

    Edit Condition Descriptor

---
+++ Source/DarkKnight/Public/DkTypes/DkStructs.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+
+#include "DkStructs.generated.h"
+
+USTRUCT()
+struct FOptionsDataEditConditionDescriptor
+{
+	GENERATED_BODY()
+
+public:
+	void SetEditConditionFunc(const TFunction<bool()>& InEditConditionFunc)
+	{
+		EditConditionFunc = InEditConditionFunc;
+	}
+
+	bool IsValid() const
+	{
+		return EditConditionFunc != nullptr;
+	}
+
+	bool IsEditableCondition() const
+	{
+		if (IsValid())
+		{
+			return EditConditionFunc();
+		}
+		return true;
+	}
+
+	FString GetDisabledRichReason() const
+	{
+		return DisabledRichReason;
+	}
+
+	void SetDisabledRichReason(const FString& InRichReason)
+	{
+		DisabledRichReason = InRichReason;
+	}
+
+	bool HasForcedStringValue() const
+	{
+		return DisabledForcedStringValue.IsSet();
+	}
+
+	FString GetDisabledForcedStringValue() const
+	{
+		return DisabledForcedStringValue.GetValue();
+	}
+
+	void SetDisabledForcedStringValue(const FString& InForcedValue)
+	{
+		DisabledForcedStringValue = InForcedValue;
+	}
+
+private:
+	TFunction<bool()> EditConditionFunc;
+	FString DisabledRichReason;
+	TOptional<FString> DisabledForcedStringValue;
+};

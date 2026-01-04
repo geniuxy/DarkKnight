e1ceb2fc8765ba66b104bbbf33a68c03ea3bfc22
geniuxy <2238384910@qq.com>
2026-01-04 21:35:02 +0800

    Key Remap Data Object

---
+++ Source/DarkKnight/Private/Widgets/Options/DataObjects/DkUIListDataObjectKeyRemap.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/Options/DataObjects/DkUIListDataObjectKeyRemap.h"
+
+void UDkUIListDataObjectKeyRemap::InitKeyRemapData(
+	UEnhancedInputUserSettings* InOwningInputUserSettings,
+	UEnhancedPlayerMappableKeyProfile* InKeyProfile,
+	ECommonInputType InDesiredInputKeyType,
+	const FPlayerKeyMapping& InOwningPlayerKeyMapping)
+{
+	CachedOwningInputUserSettings = InOwningInputUserSettings;
+	CachedOwningKeyProfile = InKeyProfile;
+	CachedDesiredInputKeyType = InDesiredInputKeyType;
+	CachedOwningMappingName = InOwningPlayerKeyMapping.GetMappingName();
+	CachedOwningMappableKeySlot = InOwningPlayerKeyMapping.GetSlot();
+}
---
+++ Source/DarkKnight/Public/Widgets/Options/DataObjects/DkUIListDataObjectKeyRemap.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonInputTypeEnum.h"
+#include "UserSettings/EnhancedInputUserSettings.h"
+#include "Widgets/Options/DataObjects/DkUIListDataObjectBase.h"
+#include "DkUIListDataObjectKeyRemap.generated.h"
+
+class UEnhancedPlayerMappableKeyProfile;
+class UEnhancedInputUserSettings;
+/**
+ * 
+ */
+UCLASS()
+class DARKKNIGHT_API UDkUIListDataObjectKeyRemap : public UDkUIListDataObjectBase
+{
+	GENERATED_BODY()
+
+public:
+	void InitKeyRemapData(
+		UEnhancedInputUserSettings* InOwningInputUserSettings,
+		UEnhancedPlayerMappableKeyProfile* InKeyProfile,
+		ECommonInputType InDesiredInputKeyType,
+		const FPlayerKeyMapping& InOwningPlayerKeyMapping
+	);
+
+private:
+	UPROPERTY(Transient)
+	UEnhancedInputUserSettings* CachedOwningInputUserSettings;
+
+	UPROPERTY(Transient)
+	UEnhancedPlayerMappableKeyProfile* CachedOwningKeyProfile;
+
+	ECommonInputType CachedDesiredInputKeyType;
+
+	FName CachedOwningMappingName;
+
+	EPlayerMappableKeySlot CachedOwningMappableKeySlot;
+};

3dee353828583f7737252cdc17ee38920495cac3
geniuxy <2238384910@qq.com>
2026-01-09 22:22:57 +0800

    Primary Interact Action

---
+++ Plugins/Inventory/Source/Inventory/Public/Controllers/InvPlayerController.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "InvPlayerController.generated.h"
+
+class UInputAction;
+class UInputMappingContext;
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_API AInvPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void SetupInputComponent() override;
+
+private:
+	void OnInteract();
+
+	UPROPERTY(EditDefaultsOnly, Category="Inventory")
+	TObjectPtr<UInputMappingContext> IMCInventory;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Inventory")
+	TObjectPtr<UInputAction> InteractAction;
+};

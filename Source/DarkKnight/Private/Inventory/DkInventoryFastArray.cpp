4f17de70339d9048689b08351480f29a6dcfa625
geniuxy <2238384910@qq.com>
2026-01-14 21:48:21 +0800

    Fast Array Serializer

---
+++ Source/DarkKnight/Private/Inventory/DkInventoryFastArray.cpp
@@ -0,0 +1,26 @@
+﻿#include "Inventory/DkInventoryFastArray.h"
+
+
+TArray<UDkInventoryItem*> FDkInventoryFastArray::GetAllItems() const
+{
+}
+
+void FDkInventoryFastArray::PreReplicatedRemove(const TArrayView<int32>& RemovedIndices, int32 FinalSize)
+{
+}
+
+void FDkInventoryFastArray::PostReplicatedAdd(const TArrayView<int32>& AddedIndices, int32 FinalSize)
+{
+}
+
+UDkInventoryItem* FDkInventoryFastArray::AddSerializerItem(UDkItemComponent* ItemComponent)
+{
+}
+
+UDkInventoryItem* FDkInventoryFastArray::AddSerializerItem(UDkInventoryItem* Item)
+{
+}
+
+void FDkInventoryFastArray::RemoveSerializerItem(UDkInventoryItem* Item)
+{
+}
---
+++ Source/DarkKnight/Public/Inventory/DkInventoryFastArray.h
@@ -0,0 +1,76 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "Net/Serialization/FastArraySerializer.h"
+
+#include "DkInventoryFastArray.generated.h"
+
+
+class UDkItemComponent;
+class UDkInventoryItem;
+class UDkInventoryComponent;
+
+/** Inventory中的单个条目 */
+USTRUCT()
+struct FDkInventoryFastArraySerializerItem : public FFastArraySerializerItem
+{
+	GENERATED_BODY()
+
+	FDkInventoryFastArraySerializerItem()
+	{
+	}
+
+private:
+	friend struct FDkInventoryFastArray;
+	friend UDkInventoryComponent;
+
+	UPROPERTY()
+	TObjectPtr<UDkInventoryItem> Item = nullptr;
+};
+
+/** Inventory中Items列表 */
+USTRUCT(BlueprintType)
+struct FDkInventoryFastArray : public FFastArraySerializer
+{
+	GENERATED_BODY()
+
+	FDkInventoryFastArray() : OwnerComponent(nullptr)
+	{
+	}
+
+	FDkInventoryFastArray(UActorComponent* InOwnerComponent) : OwnerComponent(InOwnerComponent)
+	{
+	}
+
+	TArray<UDkInventoryItem*> GetAllItems() const;
+
+	//~Begin FFastArraySerializer
+	void PreReplicatedRemove(const TArrayView<int32>& RemovedIndices, int32 FinalSize);
+	void PostReplicatedAdd(const TArrayView<int32>& AddedIndices, int32 FinalSize);
+	//~End FFastArraySerializer
+
+	bool NetDeltaSerialize(FNetDeltaSerializeInfo& DeltaParams)
+	{
+		return FastArrayDeltaSerialize<FDkInventoryFastArraySerializerItem, FDkInventoryFastArray>(
+			SerializerItems, DeltaParams, *this);
+	}
+
+	UDkInventoryItem* AddSerializerItem(UDkItemComponent* ItemComponent);
+	UDkInventoryItem* AddSerializerItem(UDkInventoryItem* Item);
+	void RemoveSerializerItem(UDkInventoryItem* Item);
+
+private:
+	friend UDkInventoryComponent;
+
+	// 复制的Items列表
+	UPROPERTY()
+	TArray<FDkInventoryFastArraySerializerItem> SerializerItems;
+
+	UPROPERTY(NotReplicated)
+	TObjectPtr<UActorComponent> OwnerComponent;
+};
+
+template <>
+struct TStructOpsTypeTraits<FDkInventoryFastArray> : public TStructOpsTypeTraitsBase2<FDkInventoryFastArray>
+{
+	enum { WithNetDeltaSerializer = true };
+};

102c43df84457b4cdbe3c40817047f02e79c17d4
geniuxy <2238384910@qq.com>
2026-01-20 21:23:46 +0800

    Hover Item

---
+++ Source/DarkKnight/Private/Widgets/Inventory/DkInventoryDraggedItem.cpp
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/Inventory/DkInventoryDraggedItem.h"
+
+#include "CommonLazyImage.h"
+#include "CommonTextBlock.h"
+#include "Inventory/DkInventoryItem.h"
+
+void UDkInventoryDraggedItem::SetImageBrush(const FSlateBrush& Brush) const
+{
+	Image_Icon->SetBrush(Brush);
+}
+
+void UDkInventoryDraggedItem::UpdateStackCount(const int32 Count) const
+{
+	if (Count > 0)
+	{
+		Text_StackCount->SetText(FText::AsNumber(Count));
+		Text_StackCount->SetVisibility(ESlateVisibility::Visible);
+	}
+	else
+	{
+		Text_StackCount->SetVisibility(ESlateVisibility::Collapsed);
+	}
+}
+
+FGameplayTag UDkInventoryDraggedItem::GetItemTag() const
+{
+	if (InventoryItem.IsValid())
+	{
+		return InventoryItem->GetItemManifest().GetItemTag();
+	}
+	return FGameplayTag();
+}
+
+void UDkInventoryDraggedItem::SetIsStackable(bool bInIsStackable)
+{
+	bIsStackable = bInIsStackable;
+	if (bInIsStackable)
+	{
+		Text_StackCount->SetVisibility(ESlateVisibility::Visible);
+	}
+	else
+	{
+		Text_StackCount->SetVisibility(ESlateVisibility::Collapsed);
+	}
+}
+
+UDkInventoryItem* UDkInventoryDraggedItem::GetInventoryItem() const
+{
+	return InventoryItem.IsValid() ? InventoryItem.Get() : nullptr;
+}
+
+void UDkInventoryDraggedItem::SetInventoryItem(UDkInventoryItem* InItem)
+{
+	InventoryItem = InItem;
+}
---
+++ Source/DarkKnight/Public/Widgets/Inventory/DkInventoryDraggedItem.h
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonUserWidget.h"
+#include "GameplayTagContainer.h"
+#include "DarkKnight/DarkKnight.h"
+#include "DkInventoryDraggedItem.generated.h"
+
+class UDkInventoryItem;
+class UCommonTextBlock;
+class UCommonLazyImage;
+/**
+ * 
+ */
+UCLASS()
+class DARKKNIGHT_API UDkInventoryDraggedItem : public UCommonUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	void SetImageBrush(const FSlateBrush& Brush) const;
+	void UpdateStackCount(const int32 Count) const;
+
+	FGameplayTag GetItemTag() const;
+
+	bool GetIsStackable() const { return bIsStackable; }
+	void SetIsStackable(bool bInIsStackable);
+
+	UDkInventoryItem* GetInventoryItem() const;
+	void SetInventoryItem(UDkInventoryItem* InItem);
+
+private:
+	//***** Bound Widgets *****//
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonLazyImage> Image_Icon;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> Text_StackCount;
+	//***** Bound Widgets *****//
+
+	TWeakObjectPtr<UDkInventoryItem> InventoryItem;
+	int32 PreviousGridIndex;
+	FIntPoint GridDimension;
+	bool bIsStackable = false;
+	int32 StackCount = 0;
+
+public:
+	LIST_DATA_ACCESSOR(int32, PreviousGridIndex)
+	LIST_DATA_ACCESSOR(FIntPoint, GridDimension)
+	LIST_DATA_ACCESSOR(int32, StackCount)
+};

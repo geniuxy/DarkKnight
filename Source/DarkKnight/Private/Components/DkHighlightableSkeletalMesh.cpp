537722854a543f30da720f21619fbdf74674cbae
geniuxy <2238384910@qq.com>
2026-02-05 21:35:42 +0800

    Creating an Equippable Item

---
+++ Source/DarkKnight/Private/Components/DkHighlightableSkeletalMesh.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/DkHighlightableSkeletalMesh.h"
+
+void UDkHighlightableSkeletalMesh::Highlight_Implementation()
+{
+	SetOverlayMaterial(HighlightMaterial);
+}
+
+void UDkHighlightableSkeletalMesh::UnHighlight_Implementation()
+{
+	SetOverlayMaterial(nullptr);
+}
---
+++ Source/DarkKnight/Public/Components/DkHighlightableSkeletalMesh.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Interfaces/HighlightInterface.h"
+#include "DkHighlightableSkeletalMesh.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DARKKNIGHT_API UDkHighlightableSkeletalMesh : public USkeletalMeshComponent, public IHighlightInterface
+{
+	GENERATED_BODY()
+
+public:
+	//~Begin IHighlightInterface Interface
+	virtual void Highlight_Implementation() override;
+	virtual void UnHighlight_Implementation() override;
+	//~End IHighlightInterface Interface
+	
+private:
+
+	UPROPERTY(EditAnywhere, Category = "Highlight")
+	TObjectPtr<UMaterialInterface> HighlightMaterial;
+};

9aee8e5e39ec331317ff4637daf64e4971266242
geniuxy <2238384910@qq.com>
2026-02-26 01:04:25 +0800

    1 - 002 - 创建基类Character（上） 碰撞预设

--- Source/DarkKnight/Private/Characters/DkCharacterBase.cpp
+++ Source/DarkKnight/Private/Characters/DkCharacterBase.cpp
@@ -10,15 +10,22 @@ ADkCharacterBase::ADkCharacterBase()
 
 	CharacterMesh = CreateDefaultSubobject<USkeletalMeshComponent>("CharacterMesh");
 	CharacterMesh->SetupAttachment(GetRootComponent());
+	CharacterMesh->SetCollisionProfileName(FName("DkMesh"));
 	
 	BodyArmorMesh = CreateDefaultSubobject<USkeletalMeshComponent>("BodyArmorMesh");
 	BodyArmorMesh->SetupAttachment(CharacterMesh);
+	BodyArmorMesh->SetCollisionProfileName(FName("DkMesh"));
 	ArmArmorMesh = CreateDefaultSubobject<USkeletalMeshComponent>("ArmArmorMesh");
 	ArmArmorMesh->SetupAttachment(CharacterMesh);
+	ArmArmorMesh->SetCollisionProfileName(FName("DkMesh"));
 	BottomArmorMesh = CreateDefaultSubobject<USkeletalMeshComponent>("BottomArmorMesh");
 	BottomArmorMesh->SetupAttachment(CharacterMesh);
+	BottomArmorMesh->SetCollisionProfileName(FName("DkMesh"));
 	HelmetMesh = CreateDefaultSubobject<USkeletalMeshComponent>("HelmetMesh");
 	HelmetMesh->SetupAttachment(CharacterMesh);
+	HelmetMesh->SetCollisionProfileName(FName("DkMesh"));
+
+	CharacterMesh->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
 
 	MotionWarping = CreateDefaultSubobject<UMotionWarpingComponent>(TEXT("MotionWarping"));
 }

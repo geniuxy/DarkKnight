0dbd0b48086338a8bd1ccd0304dc5f847a94e4b1
geniuxy <2238384910@qq.com>
2026-07-20 22:27:58 +0800

    017 - 初始化商店UI

---
+++ Source/DarkKnight/Private/Widgets/GameMenu/ShopMenu/DkWidgetShopMenuScreen.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/GameMenu/ShopMenu/DkWidgetShopMenuScreen.h"
+
+void UDkWidgetShopMenuScreen::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+}
+
+void UDkWidgetShopMenuScreen::NativeOnActivated()
+{
+	Super::NativeOnActivated();
+}
+
+void UDkWidgetShopMenuScreen::NativeOnDeactivated()
+{
+	Super::NativeOnDeactivated();
+}
---
+++ Source/DarkKnight/Public/Widgets/GameMenu/ShopMenu/DkWidgetShopMenuScreen.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/DkWidgetActivatableBase.h"
+#include "DkWidgetShopMenuScreen.generated.h"
+
+class UDkWidgetInventoryMenu;
+class UCanvasPanel;
+/**
+ * 
+ */
+UCLASS()
+class DARKKNIGHT_API UDkWidgetShopMenuScreen : public UDkWidgetActivatableBase
+{
+	GENERATED_BODY()
+
+protected:
+	//~Begin UUserWidget Function
+	virtual void NativeOnInitialized() override;
+	//~End UUserWidget Function
+
+	//~ Begin UCommonActivatableWidget Function
+	virtual void NativeOnActivated() override;
+	virtual void NativeOnDeactivated() override;
+	//~ End UCommonActivatableWidget Function
+
+private:
+	//***** Bound Widgets *****//
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget, AllowPrivateAccess="true"))
+	TObjectPtr<UCanvasPanel> CanvasPanel_ShopMenu;
+	
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget, AllowPrivateAccess="true"))
+	TObjectPtr<UDkWidgetInventoryMenu> WBP_PlayerInventoryMenu;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget, AllowPrivateAccess="true"))
+	TObjectPtr<UDkWidgetInventoryMenu> WBP_MerchantInventoryMenu;
+	//***** Bound Widgets *****//
+};

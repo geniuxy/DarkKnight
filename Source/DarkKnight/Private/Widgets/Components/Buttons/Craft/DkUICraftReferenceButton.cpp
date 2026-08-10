8a88b8be240637e1f2ac193edb5d7827ed5d57fb
geniuxy <2238384910@qq.com>
2026-08-10 21:43:02 +0800

    040 - 工艺系统列表制作, 介绍listview,制作条目控件(1)

---
+++ Source/DarkKnight/Private/Widgets/Components/Buttons/Craft/DkUICraftReferenceButton.cpp
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/Components/Buttons/Craft/DkUICraftReferenceButton.h"
+
+void UDkUICraftReferenceButton::NativeOnListItemObjectSet(UObject* ListItemObject)
+{
+	IUserObjectListEntry::NativeOnListItemObjectSet(ListItemObject);
+	
+}
+
+void UDkUICraftReferenceButton::NativeOnClicked()
+{
+	Super::NativeOnClicked();
+}
---
+++ Source/DarkKnight/Public/Widgets/Components/Buttons/Craft/DkUICraftReferenceButton.h
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonButtonBase.h"
+#include "Blueprint/IUserObjectListEntry.h"
+#include "DkUICraftReferenceButton.generated.h"
+
+class UBorder;
+class UCommonTextBlock;
+class UCommonLazyImage;
+/**
+ * 
+ */
+UCLASS()
+class DARKKNIGHT_API UDkUICraftReferenceButton : public UCommonButtonBase, public IUserObjectListEntry
+{
+	GENERATED_BODY()
+
+public:
+	//~Begin IUserObjectListEntry Interface
+	virtual void NativeOnListItemObjectSet(UObject* ListItemObject) override;
+	//~End IUserObjectListEntry Interface
+
+protected:
+	//~Begin UCommonButtonBase UFunction
+	virtual void NativeOnClicked() override;
+	//~End UCommonButtonBase UFunction
+
+private:
+	//***** Bound Widgets *****//
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget, AllowPrivateAccess="true"))
+	UCommonLazyImage* BackgroundImg;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget, AllowPrivateAccess="true"))
+	UBorder* BorderImg;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget, AllowPrivateAccess="true"))
+	UCommonLazyImage* CraftItemIcon;
+	
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget, AllowPrivateAccess="true"))
+	UCommonTextBlock* CraftItemNameText;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget, AllowPrivateAccess="true"))
+	UCommonTextBlock* CraftMessageText;
+	//***** Bound Widgets *****//
+};
